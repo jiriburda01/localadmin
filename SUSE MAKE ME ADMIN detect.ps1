aceed61bd48d7a0ad2feb0e88e7a8afd5773de72
jiriburda01 <107855000+jiriburda01@users.noreply.github.com>
2022-06-20 07:21:12 -0400

    Add files via upload

---
+++ SUSE MAKE ME ADMIN detect.ps1
@@ -0,0 +1,60 @@
+﻿$explorerprocesses = @(Get-WmiObject -Query "Select * FROM Win32_Process WHERE Name='explorer.exe'" -ErrorAction SilentlyContinue)
+If ($explorerprocesses.Count -eq 0)
+{
+    "No explorer process found / Nobody interactively logged on"
+}
+Else
+{
+    ForEach ($i in $explorerprocesses)
+    {
+        $Username = $i.GetOwner().User
+        $Domain = $i.GetOwner().Domain
+        Write-Host "$Username"
+    }
+}
+
+$user = "AzureAD\$Username"
+$group = 'Administrators'
+
+
+
+
+#Get Current UserName
+
+$CurrentUser = $user
+
+#Get User’s Local Group Membership
+net localgroup Administrators
+
+$CurrentUserGroups = net localgroup Administrators
+
+#Check if current user is a member of the Local Admins group
+
+$CurrentUserAdmin = $CurrentUserGroups -like “$user”
+
+#If user is an admin
+
+if ($CurrentUserAdmin) {
+
+write-host "user is admin"
+exit 0
+
+#Start Flow
+
+}
+
+#If user is not an admin
+
+else {
+
+
+
+#Start Flow
+
+Write-Host "User is not admin"
+exit 1618
+}
+
+
+
+
